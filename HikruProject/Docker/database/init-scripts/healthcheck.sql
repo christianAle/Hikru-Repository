ae014856693a223b5cd76c32d911ede57aca7d22
christianAle <32224978+christianAle@users.noreply.github.com>
2025-06-30 16:58:41 -0600

    Delete HikruProject directory

--- HikruProject/Docker/database/init-scripts/healthcheck.sql
+++
@@ -1,4 +0,0 @@
--- Health check script for Oracle Database
--- This script will be used by Docker healthcheck
-SELECT 1 FROM DUAL;
-EXIT;
--- HikruProject/Hikru/domain/Enums/AssessmentStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace HikruCodeChallenge.Domain.Enums;
-
-public enum AssessmentStatus
-{
-    Draft = 0,
-    Open = 1,
-    Closed = 2,
-    Archived = 3
-}
